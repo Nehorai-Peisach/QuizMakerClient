b102db104f11b968b43124d6d5ab1afc7cb4d0f0
AvihooWiener <96769936+AvihooWiener@users.noreply.github.com>
2022-02-15 16:10:07 +0200

    finally

--- src/components/editQuestion/innerComponents/answerSection.jsx
+++ src/components/editQuestion/innerComponents/answerSection.jsx
@@ -1,32 +1,84 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Answer from "./answer";
 import "../../../styles/editQuestion/innerStyles/answerSection.css";
 
 const AnswerSection = (props) => {
   const [answersState, setAnswersState] = useState([]);
-
+  const [answersList, setAnswersList] = useState([]);
+  const answersListRef = useRef({});
+  answersListRef.current = answersList;
   useEffect(() => {
     let tmpArr = [];
+    let tmpArrForList = [];
     for (let i = 0; i < 3; i++) {
-      let tmpAnswer = <Answer myIndex={i} title={`option ${i + 1}`} key={i+1} RemoveAnswer={RemoveAnswer}/>;
+      let tmpAnswer = (
+        <Answer
+          onIsCorrectChange={onIsCorrectChange}
+          onTextChange={onTextChange}
+          myIndex={i}
+          title={`option ${i + 1}`}
+          key={i}
+          // RemoveAnswer={RemoveAnswer}
+        />
+      );
+      let tmpAnswerForList = {
+        id: i,
+        text: "",
+        isCorrect: false,
+      };
       tmpArr.push(tmpAnswer);
+      tmpArrForList.push(tmpAnswerForList);
     }
-    setAnswersState(tmpArr);
+    console.log(tmpArrForList);
+    setAnswersState([...tmpArr]);
+    setAnswersList(() => {
+      return [...tmpArrForList];
+    });
   }, []);
+
   const AddAnswer = () => {
-    let length=answersState.length+1;
-    let tmpanswer=
-    (<Answer myIndex={length} title={`option ${length}`} key={length-1} RemoveAnswer={RemoveAnswer}/>);
-    setAnswersState((prevState)=>{
-      return [...prevState,tmpanswer]
+    let length = answersList.length;
+    let tmpanswer = (
+      <Answer
+        onIsCorrectChange={onIsCorrectChange}
+        onTextChange={onTextChange}
+        myIndex={length}
+        title={`option ${length + 1}`}
+        key={length}
+        // RemoveAnswer={RemoveAnswer}
+      />
+    );
+    let tmpAnswerForList = {
+      id: length,
+      text: "",
+      isCorrect: false,
+    };
+    let tmpList = answersList;
+    tmpList.push(tmpAnswerForList);
+    setAnswersState((prevState) => {
+      return [...prevState, tmpanswer];
     });
+    setAnswersList([...tmpList]);
   };
-  const RemoveAnswer = (index) => {
-    let tmpArr=answersState;
-    tmpArr.splice(index);
-    setAnswersState(()=>{
-      return [...tmpArr]
-    });
+  // const RemoveAnswer = (index) => {
+  //   let tmpArr = answersState;
+  //   tmpArr.splice(index, 1);
+  //   setAnswersState([...tmpArr]);
+  // };
+
+  useEffect(() => {
+    props.onAnswersListChange(answersList);
+  }, [answersList]);
+
+  const onIsCorrectChange = (NewIsCorrect, index) => {
+    let tmparr = answersListRef.current;
+    tmparr[index].isCorrect = NewIsCorrect;
+    setAnswersList([...tmparr]);
+  };
+  const onTextChange = (NewText, index) => {
+    let tmparr = answersListRef.current;
+    tmparr[index].text = NewText;
+    setAnswersList([...tmparr]);
   };
   return (
     <div>
--- src/components/editQuestion/innerComponents/tagsSection.jsx
+++ src/components/editQuestion/innerComponents/tagsSection.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import "../../../styles/editQuestion/innerStyles/tagsSection.css";
 
 const tagsSection = (props) => {
+  const onTagsChange = (event) => {
+    props.onTagsChange(event.target.value);
+  };
   return (
-  <div className="tagsSection__main">
+    <div className="tagsSection__main">
       <p>Tags:</p>
       <div className="tagsSection__input">
-          <input type="text" placeholder="Tags here"></input>
+        <input
+          type="text"
+          placeholder="Tags here"
+          onChange={onTagsChange}
+        ></input>
       </div>
-  </div>
-    )
+    </div>
+  );
 };
 
 export default tagsSection;
