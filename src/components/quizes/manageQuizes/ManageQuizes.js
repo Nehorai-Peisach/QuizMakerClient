3aeb3ed875aecc158bc1ed9fcd5b8ccf14adafeb
Nehorai Peisach <87582902+Nehorai-Peisach@users.noreply.github.com>
2022-02-16 22:33:22 +0200

    get

--- src/components/quizes/manageQuizes/ManageQuizes.js
+++ src/components/quizes/manageQuizes/ManageQuizes.js
@@ -1,11 +1,11 @@
 import { useMemo } from 'react';
-import makeData from 'components/helpers/MakeData';
+import { GetQuizes } from 'components/helpers/QuizesRepo';
 import QuizesTable from './QuizesTable';
 import { Columns } from './Columns';
 
 const ManageQuizes = (props) => {
   const columns = useMemo(() => Columns, []);
-  const data = useMemo(() => makeData(100000), []);
+  const data = useMemo(() => GetQuizes(), []);
 
   return <QuizesTable columns={columns} data={data} />;
 };
