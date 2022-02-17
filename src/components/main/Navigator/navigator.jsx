996163e80bee3fd7c5108c9cc8176d3bf7af4dc3
Nehorai Peisach <87582902+Nehorai-Peisach@users.noreply.github.com>
2022-02-17 13:23:02 +0200

    tables

--- src/components/main/Navigator/navigator.jsx
+++ src/components/main/Navigator/navigator.jsx
@@ -2,14 +2,14 @@ import React, { useState, forwardRef, useRef, useImperativeHandle } from 'react'
 import NewTest from 'components/quizes/newTest/newTest';
 import Home from 'components/home/Home';
 import ManageQuizes from 'components/quizes/manageQuizes/ManageQuizes';
-import ManageQustions from 'components/questions/manageQuestions/ManageQustions';
+import ManageQuestions from 'components/questions/manageQuestions/ManageQestions';
 import EditQuestion from 'components/questions/editQuestion/editQuestion';
 import ReportByStudent from 'components/reports/reportByStudent/ReportByStudent';
 import ReportByQuiz from 'components/reports/reportByQuiz/ReportByQuiz';
 import Loading from 'components/publicComponents/Loading';
 
 const Navigator = forwardRef((props, ref) => {
-  const [choosenComponent, setChoosenComponent] = useState();
+  const [choosenComponent, setChoosenComponent] = useState(<Home />);
 
   useImperativeHandle(ref, () => ({
     move(newComp) {
@@ -28,7 +28,7 @@ const Navigator = forwardRef((props, ref) => {
         setChoosenComponent(<NewTest />);
         break;
       case 'Manage Questions':
-        setChoosenComponent(<ManageQustions />);
+        setChoosenComponent(<ManageQuestions />);
         break;
       case 'Create Question':
         setChoosenComponent(<EditQuestion />);
---
+++ src/components/questions/manageQuestions/QuestionsColumns.js
@@ -0,0 +1,28 @@
+const Columns = [
+  {
+    Header: 'Id',
+    accessor: 'Id',
+  },
+  {
+    Header: 'Question',
+    accessor: 'Text',
+  },
+  {
+    Header: 'Last Update',
+    accessor: 'ChangedDate',
+  },
+  {
+    Header: 'Question Type',
+    accessor: 'Type',
+  },
+  {
+    Header: '# of Quizes',
+    accessor: 'num',
+  },
+  {
+    Header: '',
+    accessor: 'btns',
+  },
+];
+
+export default Columns;
